35cb5bb05b80e3d9b5f38bb37c52d305bfc1693e
davidxammar <david.rodriguez@inseugenixammar.cat>
2024-02-22 18:54:39 +0100

    mapes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Navegació pels menús
 
+let geoID;  // variable que guarda la localització del dispositiu
+
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
@@ -11,6 +13,17 @@ function canvia_seccio(num_boto) {
                 boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
                 boto.style.backgroundColor = "#FCDEE0";
                 seccio.style.display = "flex";    // es fa visible la secció activa
+                if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
+                    omple_llista();
+                }
+                if (num_boto == 4) {
+                    mapa.invalidateSize();
+                    marcadors();
+                    if (typeof geoID === "undefined") {    // si encara no s'han obtingut les dades de localització del dispositiu
+                        navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
+                    }
+    
+                }
             }
             else {
                 boto.style.color = "white";    // colors dels botons de seccions inactives
@@ -23,7 +36,7 @@ function canvia_seccio(num_boto) {
 
 // Inici de sessió
 
-let validat = true;    // variable que permet saber si hi ha algun usuari validat
+let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
 let scriptURL = "https://script.google.com/macros/s/AKfycbyeY1Z7_0cq2dSw_iUU2QygiGAKMTYJz_MdkKPvfeJwiiWD4yLoSFVXPdo1Qmn4ph_m/exec"    // s'ha de substituir la cadena de text per la URL del script
 
@@ -82,7 +95,9 @@ function nou_usuari() {
         });
 }
 
-// Secció 2: Càmera de fotos
+// En iniciar l'app es genera una base de dades per les fotos i un mapa per la secció 4
+
+let mapa;
 
 window.onload = () => {    // funció que s'executa un cop carregat el document HTML
     let base_de_dades = localStorage.getItem("base_de_dades");   // compte! Ha de ser localStorage o sessionStorage
@@ -110,7 +125,13 @@ window.onload = () => {    // funció que s'executa un cop carregat el document 
             }
         }
     });
+    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+    }).addTo(mapa);    // s'afegeix la capa al mapa
 }
+
+// Secció 2: Càmera de fotos
 
 function mostra_foto(id) {
     let canvas = document.getElementById("canvas");
@@ -171,6 +192,67 @@ function desa_foto() {
             alert("La foto s'ha desat correctament.");    
         };
     };
+}
+
+// Secció 3: Galeria
+
+function omple_llista() {
+    let llista = '';
+    indexedDB.open("Dades").onsuccess = event => {
+        event.target.result.transaction(["Fotos"], "readonly").objectStore("Fotos").index("Usuari_index").getAll(nom).onsuccess = event => {
+            dades = event.target.result;
+            for (i in dades) {    // per cada foto
+                llista+= '<div class="llista_fila"><div><img src="';    // es crea un contenidor de fila
+                llista+= dades[i]["Foto"];    // miniatura de la foto
+                llista+= '" onclick="mostra_foto(';    // atribut d'esdeveniment (mostrar la foto)
+                llista+= dades[i]["ID"];    // valor numèric que identifica el registre de la foto
+                llista+= ')" /></div><span>'; 
+                llista+= dades[i]["Data"];    // data i hora de la foto
+                llista+= '</span><i class="fa-solid fa-trash" onclick="esborra_foto(';    // atribut d'esdeveniment (esborrar la foto)
+                llista+= dades[i]["ID"];
+                llista+= ')"></i></div>';         
+            }
+            document.getElementById("llista_fotos").innerHTML = llista;    // s'ocupa el contenidor "llista_fotos" amb el fragment HTML creat
+        }
+    }
+}
+
+function esborra_foto(id) {
+    if (confirm("Vols esborrar la foto?")) {    // es demana la confirmació a l'usuari
+        indexedDB.open("Dades").onsuccess = event => {   
+                event.target.result.transaction("Fotos", "readwrite").objectStore("Fotos").delete(id).onsuccess = () => {
+                alert("La foto s'ha esborrat.");
+                canvia_seccio(3);    // es recarrega la galeria per tal que ja no mostri la foto esborrada
+            };
+        };
+    }
+}
+
+// Secció 4: Mapa
+
+function marcadors() {
+    L.marker([41.39, 2.17], {title:"Barcelona"}).addTo(mapa);    // l'opció "title" fa que es mostri el text "Barcelona" quan es passa el ratolí sobre el marcador
+    let vegueries = [[41.39, 2.17, "Àmbit metropolità (Barcelona)"],    // llista on cada element és una llista amb els valors de latitud, longitud i nom de vegueria com a elements
+                 [42.17, 0.89, "Alt Pirineu i Aran (Tremp)"],
+                 [41.12, 1.24, "Camp de Tarragona (Tarragona)"],
+                 [41.73, 1.83 ,"Comarques centrals (Manresa)"],
+                 [41.98, 2.82, "Comarques gironines (Girona)"],
+                 [41.62, 0.62, "Ponent (Lleida)"],
+                 [40.81, 0.52, "Terres de l'Ebre (Tortosa)"],
+                 [41.35, 1.70, "Penedès (Vilafranca del Penedès"]];
+    for (i in vegueries) {    // per cada element de la llista
+        L.marker([vegueries[i][0], vegueries[i][1]],{title:vegueries[i][2]}).addTo(mapa);
+    }
+}
+
+function geoExit(posicio){
+    let latitud = posicio.coords.latitude;
+    let longitud = posicio.coords.longitude;
+    if (typeof geoID === "undefined") {    
+        geoID = L.marker([latitud, longitud], {zIndexOffset:100, title:"Usuari"}).addTo(mapa);    // es defineix el marcador  geoID i es situa per sobre dels altres
+    } else {    // primeres dades de localització, es crea el marcador d'usuari 
+        geoID.setLatLng([latitud, longitud]);    // actualització de la posició del marcador d'usuari en el mapa
+    }
 }
 
 // Tancament de sessió
